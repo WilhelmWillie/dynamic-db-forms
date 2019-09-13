9dccca510cd4e2c16a79f647fbd8a6a6025f3d63
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-09-13 16:57:32 -0700

    Wrote additional test skeletons

--- server/test/routes/field.test.js
+++ server/test/routes/field.test.js
@@ -18,6 +18,26 @@ describe("API form", () => {
       .end((err, res) => {
         const data = res.body;
         expect(data.label).toEqual("First Name");
+        expect(data.required).toEqual(true);
+        expect(data.active).toEqual(true);
+        done();
+      });
+  });
+
+  it("PUT /api/v1/form/field/:id updates field", done => {
+    request(app)
+      .put("/api/v1/form/field")
+      .expect(200)
+      .end((err, res) => {
+        done();
+      });
+  });
+
+  it("DELETE /api/v1/form/field/:id deletes field", done => {
+    request(app)
+      .delete("/api/v1/form/field")
+      .expect(200)
+      .end((err, res) => {
         done();
       });
   });
--- server/test/routes/form.test.js
+++ server/test/routes/form.test.js
@@ -9,7 +9,25 @@ describe("API form", () => {
       .expect(200)
       .end((err, res) => {
         const data = res.body;
-        expect(data.length).toEqual(0);
+        expect(data.length).toEqual(6);
+        done();
+      });
+  });
+
+  it("POST /api/v1/form with valid input creates responses", done => {
+    request(app)
+      .post("/api/v1/form")
+      .expect(200)
+      .end((err, res) => {
+        done();
+      });
+  });
+
+  it("POST /api/v1/form with invalid input does not create responses", done => {
+    request(app)
+      .post("/api/v1/form")
+      .expect(500)
+      .end((err, res) => {
         done();
       });
   });
